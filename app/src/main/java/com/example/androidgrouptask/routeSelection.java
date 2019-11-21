ee3b5389676970ff245b76a65e6700413c7e2e1a
Joshua McBride <joshmcbride55@gmail.com>
2019-11-21 13:38:11 +0000

    routeSelectionAdded

---
+++ app/src/main/java/com/example/androidgrouptask/routeSelection.java
@@ -0,0 +1,14 @@
+package com.example.androidgrouptask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class routeSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route_selection);
+    }
+}
